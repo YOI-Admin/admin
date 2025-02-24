b1e6b723e62cb9d7a11a7b8dcb677914c27ca222
Issac Liu <issac@matchatea.dev>
2025-02-24 03:17:42 -0500

    style: update background color of dashboard for improved contrast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import AuthView from "~/app/auth/[auth]/auth-view";
 
 export default function Dashboard() {
   return (
-    <div className="flex h-screen w-screen bg-gray-900 text-white justify-center items-center">
+    <div className="flex h-screen w-screen bg-gray-800 text-white justify-center items-center">
       <AuthView />
     </div>
   );
